9ae11953ba2378ab505c3cb14af12e8646aad7a5
Oprincely <oprincely@gmail.com>
2020-07-18 16:07:22 +0100

    v1

--- wecng.py
+++ wecng.py
@@ -1,6 +1,14 @@
-from app import app, db
+from app import create_app
+
+app = create_app()
+
+
+
+'''
+from app import db
 from app.models import User
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User}
+'''
